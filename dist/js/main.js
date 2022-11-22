8074dfa9d03aceb4df007e5bdf502e15705b4d8a
Ana Markovic <markovicana@icloud.com>
2022-11-22 12:41:12 +0100

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,18 @@
+let burger = document.querySelector('.navbar__logo-mob-burger');
+let close = document.querySelector('.navbar__logo-close');
+let menu = document.querySelector('.navbar__mob-nav');
+
+burger.addEventListener('click', () => {
+  menu.classList.toggle('navbar__mob-nav--show');
+  burger.style.cssText = 'display:none;';
+  close.style.cssText = 'display:block;filter:invert(100%);height:15px;margin-bottom:-2px;';
+});
+
+close.addEventListener('click', () => {
+  menu.classList.toggle('navbar__mob-nav--show');
+  burger.style.cssText = 'display:block;';
+  close.style.cssText = 'display:none;';
+});
+
+
+
