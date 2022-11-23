5245e78a8aaef3d1048756159ea58a729b5922cb
Ana Markovic <markovicana@icloud.com>
2022-11-23 16:23:58 +0100

    prettier

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,18 +1,16 @@
-let burger = document.querySelector('.navbar__logo-mob-burger');
-let close = document.querySelector('.navbar__logo-close');
-let menu = document.querySelector('.navbar__mob-nav');
+let burger = document.querySelector(".navbar__logo-mob-burger");
+let close = document.querySelector(".navbar__logo-close");
+let menu = document.querySelector(".navbar__mob-nav");
 
-burger.addEventListener('click', () => {
-  menu.classList.toggle('navbar__mob-nav--show');
-  burger.style.cssText = 'display:none;';
-  close.style.cssText = 'display:block;filter:invert(100%);height:15px;margin-bottom:-2px;';
+burger.addEventListener("click", () => {
+  menu.classList.toggle("navbar__mob-nav--show");
+  burger.style.cssText = "display:none;";
+  close.style.cssText =
+    "display:block;filter:invert(100%);height:15px;margin-bottom:-2px;";
 });
 
-close.addEventListener('click', () => {
-  menu.classList.toggle('navbar__mob-nav--show');
-  burger.style.cssText = 'display:block;';
-  close.style.cssText = 'display:none;';
+close.addEventListener("click", () => {
+  menu.classList.toggle("navbar__mob-nav--show");
+  burger.style.cssText = "display:block;";
+  close.style.cssText = "display:none;";
 });
-
-
-
